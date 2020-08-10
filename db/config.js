9b268c83149d5eb73f8fa85c5c9740087195256f
David Anderson <d.c.anderson@live.com>
2020-08-09 23:33:20 -0400

    Testing the Index.ejs

---
+++ db/config.js
@@ -0,0 +1,24 @@
+requestAnimationFrame('dotenv').config();
+const DB_NAME = process.env.DB_NAME || "to-dos";
+
+const options= {
+    query: (e) => {
+        console.log(e.query);
+    },
+};
+
+const pgp = require('pg-promise')(options);
+
+function setDatabase() {
+    if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+        return pgp({
+            database: DB_NAME,
+            port: 5432,
+            host: 'localhost',
+        })
+    } else {
+        return pgp(process.env.DATABASE_URL)
+    }
+}
+
+module.exports = setDatabase()
