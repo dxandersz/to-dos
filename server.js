8359c1af78267e9b0aca441e374fcfb16b6a0c5c
David Anderson <d.c.anderson@live.com>
2020-08-10 00:16:11 -0400

    Server is written. User.js in process.

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const session = require('express-session'):
+
+const toDoRouter = require('./routes/user-router.js');
+
+const app = express();
+require('dotenv').config();
+
+app.use(methodOverride('_method'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        appName: 'todos',
+    });
+});
+
+app.use('/todos', todoRouter);
+
+app.use('*', (req, res) => {
+    res.status(404).send({
+        error: 'Not Found',
+    });
+});
+
+app.use(err, req, res, next) => {
+    res.status(500).send({ err, message: err.message });
+}
