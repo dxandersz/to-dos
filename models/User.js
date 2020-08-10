647b808d243781545fa86249d2cb6cb3faeec073
David Anderson <d.c.anderson@live.com>
2020-08-10 00:16:37 -0400

    Server in place, User.js in process

--- models/User.js
+++ models/User.js
@@ -6,4 +6,37 @@ class ToDo {
         this.todo = todo.todo;
         this.date = todo.date;
     }
+
+    static getAll() {
+        return db
+        .manyOrNone('SELECT * FROM todos ORDER BY id ASC')
+        .then((todos) => {
+            return todos.map((todo) => {
+                return new this (todo);
+            });
+        });
+    }
+
+    static getById(id) {
+        return db.oneOrNone('SELECT * FROM animals WHERE id = $1', id)
+        .then((todo) => {
+            if (todo) return new this(todo);
+            throw new Error(`You haven't made this task.`);
+        });
+    }
+
+    save() {
+        return db.one(
+            `INSERT INTO todos todo, date)
+            VALUES ($/todo/, $/date/ RETURNING *`, this)
+        .then((todo) => {
+            return Object.assign(this, todo);
+        })
+    }
+
+    update(changes) {
+        
+    }
+
+
 }
